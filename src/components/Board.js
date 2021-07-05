9ffd02d8a13d5e6818846c3bbf63d464e5b260ea
WYang6060 <wyang6060@outlook.com>
2021-07-05 17:16:49 +0200

    build constructure

---
+++ src/components/Board.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Square from './Square';
+
+function Board() {
+
+}
+
+export default Board;
---
+++ src/components/Game.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import Board from './Board';
+
+function Game() {
+
+}
+
+export default Game;
